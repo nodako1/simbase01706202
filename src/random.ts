026029e0c945b05df75ff7312a97264fbdc6cb7f
KOICHI NODA <79571927+nodako1@users.noreply.github.com>
2026-07-11 01:46:49 +0900

    feat: add deterministic random generator

---
+++ src/random.ts
@@ -0,0 +1,26 @@
+export class SeededRandom {
+  private state: number;
+
+  constructor(seed: number) {
+    this.state = seed >>> 0 || 1;
+  }
+
+  next(): number {
+    let t = (this.state += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4_294_967_296;
+  }
+
+  range(min: number, max: number): number {
+    return min + (max - min) * this.next();
+  }
+
+  integer(min: number, maxInclusive: number): number {
+    return Math.floor(this.range(min, maxInclusive + 1));
+  }
+
+  angle(): number {
+    return this.range(0, Math.PI * 2);
+  }
+}
